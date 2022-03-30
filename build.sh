997c66d36b9a08ad74b65e258e4ce8da8a612196
Mark Hiner <hinerm@gmail.com>
2022-03-30 14:53:57 -0500

    Remove need for build success caching
    Instead of caching the times the update sites were checked or the
    Fiji archives built, we now just always grab these times from the
    updates sites and compare with the fiji-no-jre bundle. This should
    result in less transfer of data per build than caching while also being
    simpler.

--- build.sh
+++ build.sh
@@ -1,72 +1,35 @@
 #!/bin/bash
 set -e
 
-CACHE_DIR=cache
-FIJI_HOME="$CACHE_DIR/Fiji.app"
+if [ "$CI" = "true" ]
+then
+	# Running on CI. Use a base off the home directory.
+	BASE_DIR="$HOME"
+else
+	# Running locally. Use a base in the current directory.
+	BASE_DIR="."
+fi
+
+FIJI_HOME="$BASE_DIR/Fiji.app"
 
 echo
 echo "== Checking whether anything has changed =="
 
-# Get last modified date from header.
-repos=( "imagej" "sites" "fiji")
-date1=`curl -svX HEAD https://update.imagej.net/db.xml.gz 2>&1 | grep 'Last-Modified:'`
-date2=`curl -svX HEAD https://sites.imagej.net/Java-8/db.xml.gz 2>&1 | grep 'Last-Modified:'`
-date3=`curl -svX HEAD https://update.fiji.sc/db.xml.gz 2>&1 | grep 'Last-Modified:'`
-# Trim string to just the date.
-date1="${date1#*, }"
-date2="${date2#*, }"
-date3="${date3#*, }"
-# Convert date to seconds since the epoch.
-case "$(uname)" in
-  Darwin)
-    date1=`date -j -f '%d %b %Y %H:%M:%S %Z ' "$date1" +%s`
-    date2=`date -j -f '%d %b %Y %H:%M:%S %Z ' "$date2" +%s`
-    date3=`date -j -f '%d %b %Y %H:%M:%S %Z ' "$date3" +%s`
-    ;;
-  *)
-    date1=`date --date="$date1" +%s`
-    date2=`date --date="$date2" +%s`
-    date3=`date --date="$date3" +%s`
-    ;;
-esac
-dates=( "$date1" "$date2" "$date3" )
+# the archives-updated script will return 'false' if an update is needed
+up_to_date="$(./archives-updated.sh)"
 
-# Read previous dates. The first time, there won't be any.
-changes=false
-datesFile="$CACHE_DIR/dates.txt"
-if [ ! -e "$datesFile" ];
-then
-  # Save the first dates
-  echo "Running for the first time, no dates to compare with, assuming changes exist."
-  mkdir -p "$CACHE_DIR"
-  changes=true
-else
-  # Compare to previous cached dates, then save latest dates.
-  echo "Comparing entries"
-  i=0
-  while IFS= read -r line
-  do
-    echo "Comparing new $line to previous ${dates[$i]}"
-    if [[ $line -lt ${dates[$i]} ]];
-    then
-      echo "There are updates in the ${repos[$i]} site. New distros will be generated."
-      changes=true
-    fi
-    i=$i+1
-  done <"$datesFile"
-fi
-
-if [ "$changes" = false ]; then
-   echo "Nothing has changed. No distros will be generated."
-   exit 0
+if [ "$up_to_date" = "up-to-date" ]; then
+	echo "Nothing has changed. No distros will be generated."
+	exit 0
 fi
 
 # Initialize the Fiji.app installation if needed.
-test -d "$FIJI_HOME" || {
+if [ ! -d "$FIJI_HOME" ]
+then
   echo
   echo "== Building Fiji installation =="
   ./bootstrap-fiji.sh "$FIJI_HOME" || exit 1
-}
+fi
 
 # Update the Fiji.app installation.
 echo
@@ -82,8 +45,3 @@ echo "== Generating archives =="
 echo
 echo "== Transferring artifacts =="
 ./upload-archives.sh || exit 4
-
-# Finally, since everything worked OK, save the new dates.
-echo "${dates[0]}" > "$datesFile"
-echo "${dates[1]}" >> "$datesFile"
-echo "${dates[2]}" >> "$datesFile"
