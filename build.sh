c7b68f1e30e2640c9fd6c43cdd0a7c6d58e2eda2
Curtis Rueden <ctrueden@wisc.edu>
2022-04-14 17:38:37 -0500

    build.sh: add case logic for BSD date
    This code will disappear soon anyway, but just for now,
    to avoid needing to manually edit when running on macOS.

--- build.sh
+++ build.sh
@@ -16,13 +16,19 @@ date3=`curl -svX HEAD https://update.fiji.sc/db.xml.gz 2>&1 | grep 'Last-Modifie
 date1="${date1#*, }"
 date2="${date2#*, }"
 date3="${date3#*, }"
-# Convert date to seconds since the epoch (commented code is MacOS version).
-#date1=`date -j -f '%d %b %Y %H:%M:%S %Z ' "$date1" +%s`
-#date2=`date -j -f '%d %b %Y %H:%M:%S %Z ' "$date2" +%s`
-#date3=`date -j -f '%d %b %Y %H:%M:%S %Z ' "$date3" +%s`
-date1=`date --date="$date1" +%s`
-date2=`date --date="$date2" +%s`
-date3=`date --date="$date3" +%s`
+# Convert date to seconds since the epoch.
+case "$(uname)" in
+  Darwin)
+    date1=`date -j -f '%d %b %Y %H:%M:%S %Z ' "$date1" +%s`
+    date2=`date -j -f '%d %b %Y %H:%M:%S %Z ' "$date2" +%s`
+    date3=`date -j -f '%d %b %Y %H:%M:%S %Z ' "$date3" +%s`
+    ;;
+  *)
+    date1=`date --date="$date1" +%s`
+    date2=`date --date="$date2" +%s`
+    date3=`date --date="$date3" +%s`
+    ;;
+esac
 dates=( "$date1" "$date2" "$date3" )
 
 # Read previous dates. The first time, there won't be any.
