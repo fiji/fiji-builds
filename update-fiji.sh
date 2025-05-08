b5b0f46bfdd359a551dcba2f96431a8bea90d805
Curtis Rueden <ctrueden@wisc.edu>
2025-05-08 17:47:58 -0500

    WIP4 2025-05-08

--- update-fiji.sh
+++ update-fiji.sh
@@ -16,3 +16,7 @@ rm -rf \
   "$fiji_dir/fiji-linux64" \
   "$fiji_dir/fiji-win32.exe" \
   "$fiji_dir/fiji-win64.exe"
+
+# Remove any backup launcher-related files.
+find . -name '*.old' -exec rm "{}" \;
+find . -name '*.old.exe' -exec rm "{}" \;
