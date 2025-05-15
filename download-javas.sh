177fe6487dece8182dbc5446a9bf7193ce545b38
Curtis Rueden <ctrueden@wisc.edu>
2025-05-15 16:55:30 -0500

    Fix Java structure for stable macosx bundles
    The ImageJ launcher really wants to find libjli.dylib at:
    - java/macosx/<distro>/jre/Contents/Home/lib/jli/libjli.dylib
    
    But for Zulu OpenJDK 8, neither the JDK nor the JRE flavor has exactly
    this directory structure. So we hack in a symlink for each to fix it.

--- download-javas.sh
+++ download-javas.sh
@@ -31,6 +31,48 @@ for platform in $platforms; do
           esac
           rm "$javaArchive"
         )
+        if [ "$track" = stable -a "$platform" = macosx ]; then
+          echo "--> Hacking macosx $jtype to appease the ImageJ launcher"
+          # On macOS, the ImageJ launcher expects to find:
+          # - java/macosx/<distro>/jre/Contents/Home/lib/jli/libjli.dylib
+          # - java/macosx/<distro>/jre/Contents/Home/lib/server/libjvm.dylib
+          (
+            cd "$targetDir"/*/
+            case "$jtype" in
+              jre)
+                # Within java/macosx/<distro>, the Zulu JRE distro has:
+                # - zulu-8.jre/Contents/Home/lib/jli/libjli.dylib
+                # - zulu-8.jre/Contents/Home/lib/server/libjvm.dylib
+                #
+                # So it's almost correct; the jre folder is just named
+                # `zulu-8.jre` rather than `jre`. A symlink fixes it.
+                ln -s *.jre jre
+                ;;
+              jdk)
+                # Within java/macosx/<distro>, the Zulu JDK distro has:
+                # - zulu-8.jdk/Contents/Home/jre/lib/jli/libjli.dylib
+                # - zulu-8.jdk/Contents/Home/jre/lib/server/libjvm.dylib
+                # - jre -> zulu-8.jdk/Contents/Home/jre
+                #
+                # and therefore (via the jre symlink):
+                # - jre/lib/jli/libjli.dylib
+                # - jre/lib/server/libjvm.dylib
+                #
+                # Consequently, the paths do not match.
+                #
+                # To fix this, we symlink a nested Contents/Home:
+                # - jre/Contents/Home -> jre
+                #
+                # Yes, it's an infinite directory loop, but the ImageJ
+                # Launcher does not recurse infinitely, so it's fine.
+                cd jre
+                mkdir Contents
+                cd Contents
+                ln -s .. Home
+                ;;
+            esac
+          )
+        fi
       else
         echo "--> Skipping $javaBundle for $platform: no Java bundle URL found"
       fi
